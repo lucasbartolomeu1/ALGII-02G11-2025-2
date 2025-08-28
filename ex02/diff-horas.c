14ee72c1da027c658bb0be472fcda7fbb038a9c6
Lucas Bartolomeu <lucasdossantosbartolomeu36@gmail.com>
2025-08-28 18:50:37 +0000

    create ex02

--- ex02/diff-horas.c
+++ ex02/diff-horas.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 
-void minutos (int *hr1, int *hr2, int *min1, int *min2) {
-    int *hr1 = (hr1*60) + min1;
-    int *hr2 = (hr2*60) + min2;
+int minutos(int hora, int minutos) {
+    return (hora * 60) + minutos;
 }
 
 
+int diferenca(int tempo_minutos1, int tempo_minutos2) { 
+    int resultado = tempo_minutos1 - tempo_minutos2;
+    if (resultado < 0) {
+        return resultado * -1; 
+    }
+    else {
+        return resultado;
+    }
+}
+
 int main() {
-    int hr1, min1, hr2, min2;
-    printf("Hora 1: ");
-    scanf("%d:%d", &hr1, &min1);
-    printf("Hora 2: ");
-    scanf("%d:%d", &hr2, &min2);
-    minutos (&hr1, &hr2, &min1, &min2);
+    int hora1, minuto1;
+    int hora2, minuto2;
+    printf("Primeira hora (formato hr:min): ");
+    scanf("%d:%d", &hora1, &minuto1);
+    printf("Segunda hora (formato hr:min): ");
+    scanf("%d:%d", &hora2, &minuto2);
+
+    int total_minutos1 = minutos(hora1, minuto1);
+    int total_minutos2 = minutos(hora2, minuto2);
+
+    int resultado = diferenca(total_minutos1, total_minutos2);
+    printf("A diferença entre os horários é de: %d minutos\n", resultado);
+    return 0;
 }
